e978a50be0085d1dd80dff80fa5b2e38b9674ce4
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2014-09-25 19:01:24 +0200

    Added CORS for testing in localhost

--- Node.js/server.js
+++ Node.js/server.js
@@ -1,6 +1,7 @@
 // ---------- Requires
 var express = require('express');
 var https = require('https');
+var http = require('http');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -12,6 +13,22 @@ var auth = require('./utils/authentication');
 var app = express();
 app.use(bodyParser());
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X_USERNAME, X_PASSWORD');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
+
 // ------------ Errors
 app.use(function(err, req, res, next) {
     if (err) {
@@ -21,8 +38,14 @@ app.use(function(err, req, res, next) {
     else next(); 
 });
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 // -------------- Base de Dades
-mongoose.connect('mongodb://user_reserves:pass_reserves@localhost/reserves', function(err) {
+mongoose.connect('mongodb://localhost/reservas', function(err) {
     if (err) {
         console.log(new Date().toISOString(), 'Error de connexió a Base de Dades. Veure log');
         console.error(new Date().toISOString(), err);
@@ -71,11 +94,15 @@ app.use('/api', router);
 
 // ---------- Server
 var server = https.createServer({
-    key: fs.readFileSync('./https/key.pem'), 
+    key: fs.readFileSync('./https/key.pem'),
     cert: fs.readFileSync('./https/cert.pem')
 }, app);
 
+/*http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});*/
+
 var port = process.env.PORT || 8080;
-//app.listen(port); //--> HTTP
-server.listen(port); // --> HTTPS 
+app.listen(port); //--> HTTP
+//server.listen(port); // --> HTTPS
 console.log(new Date().toISOString(), 'API Reserves, port ' + port);
